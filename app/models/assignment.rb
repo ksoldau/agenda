20871791fe91134b49befa9aaf531c5996753773
Katie Soldau <ksoldau@gmail.com>
2013-05-06 13:43:00 -0400

    sort assignments by due date in db

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,6 +1,9 @@
 class Assignment < ActiveRecord::Base
   # attr_accessible :title, :body
   belongs_to :user
-  
- validates :description, :due_date, :user_id, :presence => true 
+
+  validates :description, :due_date, :user_id, :presence => true
+
+  #to order assignemnts by due date (closest due date is first)
+  default_scope order('due_date')
 end
