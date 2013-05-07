05da576a137f606900482693e2daf1062dd767aa
Katie Soldau <ksoldau@gmail.com>
2013-05-07 13:55:35 -0400

    mved a stylesheet

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -2,7 +2,7 @@ class Assignment < ActiveRecord::Base
   # attr_accessible :title, :body
   belongs_to :user
 
-  validates :description, :due_date, :user_id, :presence => true
+  validates :description, :due_date, :subject, :user_id, :presence => true
 
   #to order assignemnts by due date (closest due date is first)
   default_scope order('due_date')
