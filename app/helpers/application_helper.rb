7ac44bed63f272a8e5e675cbe1ef71a8e39ec8c0
Katie Soldau <ksoldau@gmail.com>
2013-05-08 12:43:22 -0400

    fix sunday date and day

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,12 +2,17 @@ module ApplicationHelper
   def getDate(n, beginning_of_week)
     $i = -1
     $day = beginning_of_week
-    #binding.pry
-     until $i > n
+
+    #key for Sunday is 0, so have to change it 
+    if n == 0 
+      n = 7
+    end
+
+    #get correct date 
+    until $i > n
        $day = $day.tomorrow
        $i = $i + 1
      end
     return $day
   end
-  #binding.pry
 end
