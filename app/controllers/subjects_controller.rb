2b7e8137d3bd9cffcd2922a1d5274c08054f4431
Katie Soldau <ksoldau@gmail.com>
2013-05-10 11:40:54 -0400

    create subject controller

---
+++ app/controllers/subjects_controller.rb
@@ -0,0 +1,29 @@
+class SubjectsController < ApplicationController
+  before_filter :authenticate_user!
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def edit 
+  end
+
+  def destroy
+  end
+
+ 
+####################################################### 
+ 
+end
