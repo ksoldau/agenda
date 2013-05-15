b67ae8a82a1859c9694aae4295855d961022b313
Katie Soldau <ksoldau@gmail.com>
2013-05-15 16:37:58 -0400

    delete default width for assignment dialogs in month

--- app/assets/javascripts/assignment_dialog.js
+++ app/assets/javascripts/assignment_dialog.js
@@ -1,20 +1,17 @@
  
  function initializeAssignmentsDialogs() {
- console.log("it got to initialize assignmentsdiolags");
   $(".subj_link").each(
     function() {
       var dialog = $(this).find(".a_dialog").dialog({
        //open: function(event, ui) { $(".ui-dialog-titlebar-close").show(); },
         closeOnEscape: true,
         title: "Assignment",
-        width: 300,
         draggable: true,
         resizable: false,
         modal: false,
         autoOpen: false,
         });
 
-      console.log("got into the each");
       $(this).on('click', function() {
         dialog.dialog('open');
       });
