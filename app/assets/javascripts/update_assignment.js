5e274746d69dae0f19ea1d7daa9647ce61ace2e2
Katie Soldau <ksoldau@gmail.com>
2013-05-15 16:47:33 -0400

    add (commented out) stuff about ajax and regular expressions

---
+++ app/assets/javascripts/update_assignment.js
@@ -0,0 +1,33 @@
+/* $('<update button>').on('click', function(e) { */
+/*   e.preventDefault(); */
+
+/*   url = $('form').attr('action'); */
+/*   subject_id = ... */
+/*   description = ... */
+/*   due_date = ... */
+
+/*   $.ajax({ */
+/*     type: "PUT", */ 
+/*     url: <url>, */ 
+/*     data: { assignment: { */
+/*       subject_id: subject_id, */ 
+/*       description: description, */ 
+/*       due_date: due_date */
+/*     }}, */
+/*     dataType: 'json' */
+/*     success: function(data) { */
+/*       //manually update dom */
+/*       //close modal */
+/*     } */
+
+
+/*   }); */
+/* }) */
+
+/* /1* a a char *1/ */
+/* /1* . any char *1/ */
+/* /1* a* zero or more *1/ */
+/* /1* a? zerp pr oen *1/ */
+/* /1* a+ one or more *1/ */
+/* /1* | or *1/ */
+
