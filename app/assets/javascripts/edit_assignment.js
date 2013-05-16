9aea9f893536ca6807ef47915e3255a726067fe7
Katie Soldau <ksoldau@gmail.com>
2013-05-16 14:30:33 -0400

    change attributes with ajax when edit assignment

--- app/assets/javascripts/edit_assignment.js
+++ app/assets/javascripts/edit_assignment.js
@@ -10,11 +10,23 @@ $(function() {
       console.log("this is data: %o", data);
       console.log("ajax success");
       console.log("assignment successfully edited");
-      dialog.dialog('close', {duration: 10});
+      dialog.dialog('close');
       //console.log("data.fdsaf
       console.log(data.completed);
-      trigger.closest(".as_box").addClass(data.completed ? "completed" : "not_completed", {duration: 1000}
-      ).removeClass(data.completed ? "not_completed" : "completed", {duration: 1000});
+
+      
+      
+      //change subject based on edit
+      trigger.closest(".as_box").find(".subject").first().text(data.subject.name);
+      //change description based on edit
+      trigger.closest(".as_box").find(".description").first().text(data.description);
+      //change due time based on edit
+      trigger.closest(".as_box").find(".due_time").first().text(data.due_date)
+
+
+      //change background color based on completion 
+      trigger.closest(".as_box").addClass(data.completed ? "completed" : "not_completed", {duration: 800}
+      ).removeClass(data.completed ? "not_completed" : "completed", {duration: 800});
    }).on('ajax:failure', function(e) {
     console.log("assignment edit FAILED");
     });
