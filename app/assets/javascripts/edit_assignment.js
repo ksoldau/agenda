748b52595ab2cb020219983aa42e3d81878db700
Katie Soldau <ksoldau@gmail.com>
2013-05-20 15:23:29 -0400

    fix dynamically moving assignments based on edits

--- app/assets/javascripts/edit_assignment.js
+++ app/assets/javascripts/edit_assignment.js
@@ -14,13 +14,16 @@ $(function() {
       
       //change subject if edited
       var subj = trigger.closest(".as_box").find(".subject").first();
+      
       if (subj.text().trim() !== data.subject.name.trim()) {
          subj.animate({opacity: "0"}, 700, function() {
           subj.text(data.subject.name).animate({opacity: "1"}, 700);
         });
       }
+
       //change description based on edit
       var desc = trigger.closest(".as_box").find(".description").first()
+      
       if (desc.text().trim() !== data.description.trim()) {
         desc.animate({opacity: "0"}, 700, function() {
           desc.text(data.description).animate({opacity: "1"}, 700);
@@ -28,115 +31,162 @@ $(function() {
       }
 
       //change of placement based on time edit
-      var dd = data.due_date;
-      var new_parsed = dd.split('T')[0].split('-');
-      var new_parsed_time = dd.split('T')[1].split(':');
-
-      var url = window.location.href
-      var url_parsed = url.split('=')[2].split('-');
-
+      // make sure new day, min, and hour are all two digits
+      var new_dd = data.due_date;
+      var new_parsed_date = new_dd.split('T')[0].split('-');
+      var new_parsed_time = new_dd.split('T')[1].split(':');
       var new_min = Number(new_parsed_time[1]);
       var new_hour = Number(new_parsed_time[0]);
-      var new_day = Number(new_parsed[2]);
-      var new_month = Number(new_parsed[1]) -1; //0 is jan
-      var new_year = Number(new_parsed[0]);
-      var new_date = new Date(new_year, new_month, new_day)
-      
+      var new_day = Number(new_parsed_date[2]);
+      var new_month = Number(new_parsed_date[1]) -1; //0 is jan
+      var new_year = Number(new_parsed_date[0]);
+      var new_date = new Date(new_year, new_month, new_day);
+
+      var url = window.location.href;
+      var url_parsed = url.split('=')[2].split('-');
       var url_day = Number(url_parsed[2]);
       var url_month = Number(url_parsed[1]) -1;
       var url_year = Number(url_parsed[0]);
-      var url_date = new Date(url_year, url_month, url_day)
+      var url_date = new Date(url_year, url_month, url_day);
       var url_date_end_week = new Date(url_date.getTime() + 6 * 24 * 60 * 60 * 1000);
 
+      assignment = trigger.closest(".as_box");
+
+      old_date = assignment.closest(".assignment_box").find(".day_heading").text().trim();
+      old_year = Number(old_date.match(/\d+/g)[1]);
+      old_day = Number(old_date.match(/\d+/g)[0]);
+
+      old_time = assignment.find(".due_time").text().trim();
+      old_hour = Number(old_time.split(':')[0]);
+      old_min = Number(old_time.split(':')[1]);
       
+      var old_date_and_time = new Date(old_year, 0, old_day, old_hour, old_min);
+      var new_date_and_time = new Date(new_year, 0, new_day, new_hour, new_min);
+
+      var old_date = new Date(old_year, 0, old_day);
+      var new_date_no_month = new Date(new_year, 0, new_day);
+
+
+      // if date moved to before week
       if (new_date.getTime() < url_date.getTime()) {
         trigger.closest(".as_box").animate({left: '-999px'}, 800, function() {
           trigger.closest(".as_box").slideUp();
         });
       }
+
+      // if date moved to after week
       else if (new_date.getTime() > url_date_end_week.getTime()) {
         trigger.closest(".as_box").animate({left: '999px'}, 800, function() {
           trigger.closest(".as_box").slideUp();
         });
       }
-      else if (url_date.getTime() <= new_date.getTime() &&
-        new_date.getTime() <= url_date_end_week.getTime()) {
-        console.log("got here");
-        //assuming position has to change
-        
-        var new_day_string = String(new_day);
-        var headingArray = $(".day_heading");
+      
+      // if due date and time have not changed
+      else if (new_date_and_time.getTime() === old_date_and_time.getTime()) {
+        //do nothing
+      }
+      
+      // if date has not changed 
+      else if (old_date.getTime() === new_date_no_month.getTime()) {
+        //code not written yet
 
-        for (var i = 0; i < headingArray.length; i++) {
-          console.log("got hereeee");
-          h = headingArray[i];
+        //find new time
+        new_time = new Date (0, 0, 1, new_hour, new_min);
+
+        //find prev time
+        //find next time
+
+
+
+      }
+
+      //if date has changed
+      else if (old_date.getTime() !== new_date_no_month.getTime()) {
+        //old assignment
+        assignment = trigger.closest(".as_box");
+        assignment.animate({opacity: '0'}, 800, function() {
+          assignment.slideUp(600, function (e) {
+
+        //figure out where to put new assignment
+        var new_day_string = String(new_day);
+        
+        var dayHeadingArray = $(".day_heading");
+        for (var i = 0; i < dayHeadingArray.length; i++) {
+          h = dayHeadingArray[i];
           htext = $(h).text();
 
-          //if in right day
+          // is it in the right day?
           if (htext.indexOf(new_day_string + ",") >= 0) {
-            console.log("it's in " + new_day_string);
-          }
-            
-            var day = $(h).closest(".assignment_box");
+            console.log("in this if");
+            var new_ab = $(h).closest(".assignment_box");
             var assignment = trigger.closest(".as_box");
 
-            var dt = assignment.find(".due_time").text();
-            console.log(dt);
-            var dt_hour = dt.split(':')[0];
-            var dt_min = dt.split(':')[1];
-            var dt_already = new Date(0, 0, 1, dt_hour, dt_min);
-            console.log("dt_hour is %o and dt_min is %o", dt_hour, dt_min);
-            var new_dt = new Date(0, 0, 1, new_hour, new_min);
+            var assignmentsArray = new_ab.find(".as_box");
+            console.log("length of assignmentsArray is %o", assignmentsArray.length);
 
-            if (dt_already.getTime() !== new_dt.getTime()) {
+            //
+            assignment.appendTo(new_ab);
+            
+            var aArray = Array.prototype.slice.call(assignmentsArray);
+            
+            if (aArray.length > 0) {
+              for (i = 0; i < aArray.length; i++) { 
+                var o_a = $(aArray[i]);
+                var o_due_time = o_a.find(".due_time");
+                var o_due_time_text = o_due_time.text().replace(/\s+/g, '');
+                var o_hour = Number(o_due_time_text.split(':')[0]);
+                var o_min = Number(o_due_time_text.split(':')[1]);
+                var o_date = new Date (0, 0, 1, o_hour, o_min);
+                console.log(o_hour);
+                console.log(o_min);
+                console.log(o_date.getTime());
+                
+                if (new_hour < 10) {
+                  new_hour = "0" + new_hour;
+                }
+                if (new_min < 10) {
+                  new_min = "0" + new_min;
+                }
+                var new_date_time = new Date(0, 0, 1, Number(new_hour), Number(new_min));
+                console.log(new_date_time.getTime());
 
-              assignment.animate({opacity: '0'}, 800, function() {
-                assignment.slideUp(600, function () {
-                  assignment.appendTo(day);
-                  
-                  var assignmentsArray = day.find(".as_box");
-                  
-                  outer_loop:
-                  for (var i = 0; i <assignmentsArray.length; i++) {
-                    console.log("in the loop");
-                    var o_a = $(assignmentsArray[i]);
-                    var o_due_time = o_a.find(".due_time");
-                    var o_due_time_text = o_due_time.text().replace(/\s+/g, '');
-                    var o_hour = Number(o_due_time_text.split(':')[0]);
-                    var o_min = Number(o_due_time_text.split(':')[1]);
-                    console.log(o_hour);
-                    console.log(o_min);
-                    var o_date = new Date(0, 0, 1, o_hour, o_min);
+                if (new_date_time.getTime() <= o_date.getTime()) {
+                  console.log("GOT INTO THIS IF");
+                  o_a.before(assignment);
+                }
+              }; //end of for
+            }
 
-                    var new_date_time = new Date(0, 0, 1, new_hour, new_min);
+            assignment.slideDown(600, function() {
+              assignment.animate({opacity: '1'}, 600);
+            });
 
-                    if (new_date_time.getTime() <= o_date.getTime()) {
-                      o_a.before(assignment);
-                      break outer_loop;
-                    }
-                  }
-
-                  assignment.slideDown(600, function() {
-                    assignment.animate({opacity: '1'}, 800);
-                  });
-
-                });   
-              });
-          }; //close the if
-        }
+          } // end of if
         
-        }
+        } 
+
+
+          });
+        });
+        
+
+
+      }
 
       //change due time based on edit
       //change to have it only change if time changed
-
+      if (new_hour < 10) {
+        new_hour = "0" + new_hour;
+      }
+      if (new_min < 10 ) {
+        new_min = "0" + new_min;
+      }
       trigger.closest(".as_box").find(".due_time").first().text(new_hour + ":" + new_min);
       
    
       //change background color based on completion 
       trigger.closest(".as_box").addClass(data.completed ? "completed" : "not_completed", {duration: 800}
-      ).removeClass(data.completed ? "not_completed" : "completed", {duration: 800});
-   }).on('ajax:failure', function(e) {
+      ).removeClass(data.completed ? "not_completed" : "completed", {duration: 800});}).on('ajax:failure', function(e) {
     console.log("assignment edit FAILED");
     });
   });
