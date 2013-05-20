964f9863088aeaee1d67ddce011ffa7eb015ffd3
Katie Soldau <ksoldau@gmail.com>
2013-05-20 16:47:48 -0400

    added alert if double click day in calendar

---
+++ app/assets/javascripts/calendar.js
@@ -0,0 +1,13 @@
+$(function() {
+  $("td").each(
+    function() {
+      $(this).dblclick(function() {
+        alert('Handler for .dblclick() called.');
+        // put in dialog to add assignment to the day
+      });
+    }
+  )
+});
+
+
+
