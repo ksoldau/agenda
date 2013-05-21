a09551687ff7fe8e579b0d7fba1fd7accb3f3158
Katie Soldau <ksoldau@gmail.com>
2013-05-21 16:49:29 -0400

    open assignment dialog near subj link

--- app/assets/javascripts/assignment_dialog.js
+++ app/assets/javascripts/assignment_dialog.js
@@ -2,7 +2,7 @@
  function initializeAssignmentsDialogs() {
   $(".subj_link").each(
     function() {
-      var dialog = $(this).find(".a_dialog").dialog({
+      var dlg = $(this).find(".a_dialog").dialog({
        //open: function(event, ui) { $(".ui-dialog-titlebar-close").show(); },
         closeOnEscape: true,
         title: "Assignment",
@@ -13,8 +13,29 @@
         });
 
       $(this).on('click', function() {
-        dialog.dialog('open');
+        dlg.dialog('open');
       });
+
+      /* $(this).mouseover( function() { */
+      /*   dialog.dialog('open'); */
+      /* }); */
+      /* $(this).mouseout( function() { */
+      /*   dialog.dialog('close'); */
+      /* }); */
+
+    $(this).mouseover(function() {
+      dlg.dialog("open");
+    }).mousemove(function(event) {
+      /* dlg.dialog("option", "position", { */
+      /*   my: "left top", */
+      /*   at: "right bottom", */
+      /*   of: event, */
+      /*   offset: "200 2000 200 200" */
+      /* }); */
+      dlg.dialog("option", "position", [event.clientX - 150, event.clientY - 170]);
+    }).mouseout(function() {
+      dlg.dialog("close");
+    });
 
     });
   }
