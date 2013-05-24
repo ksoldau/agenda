98665fafadad1478f76caf6dd6454248b314025e
Katie Soldau <ksoldau@gmail.com>
2013-05-24 16:42:58 -0400

    make drop down change what you can see

---
+++ app/assets/javascripts/subjects.js
@@ -0,0 +1,21 @@
+$(function() {
+  $(".dk_container .dk_options li a").each(
+    function() {
+
+      $(this).on('click', function() {
+        var idd = $(this).data("dk-dropdown-value")
+
+        $(".dk_container .dk_options li a").each(
+          function() {
+          var iddd = $(this).data("dk-dropdown-value");
+
+          $("#" + iddd).attr("aria-expanded", "false").attr("aria-hidden", "true");
+          $("#" + iddd).attr("style", "display:none");
+        });
+
+        $("#" + idd).attr("aria-expanded", "true").attr("aria-hidden", "false");
+        $("#" + idd).attr("style", "display:block");
+
+      });
+    });
+});
