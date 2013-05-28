da22a45bce484610be12f6ecad1397826a9d3995
Katie Soldau <ksoldau@gmail.com>
2013-05-28 16:20:21 -0400

    fix drop down subjects to show current one

--- app/assets/javascripts/subjects.js
+++ app/assets/javascripts/subjects.js
@@ -21,3 +21,30 @@ $(function() {
       });
     });
 });
+
+
+// switching tab view to right state
+// switching drop down to right thing based on tab click in case screen size changes
+$(function() {
+  $("li[role=tab]").each ( function() {
+    $(this).on('click', function() {
+      var subjectName = $(this).find("a").text().trim();
+      console.log(subjectName);
+      $(".dk_container .dk_toggle .dk_label").text(subjectName);
+
+      // remove all current states from 
+      $(".dk_options .dk_options_inner li").each( function() {
+        $(this).removeClass("dk_option_current");
+      });
+      
+      // add current state to correct drop down option
+      $(".dk_options .dk_options_inner li").each ( function () {
+        var a = $(this).find("a").text().trim();
+        console.log(a);
+        if (a.indexOf(subjectName) >= 0) {
+          $(this).addClass("dk_option_current");
+        }
+      });
+    });
+  });
+});
