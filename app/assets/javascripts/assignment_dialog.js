d3e22da895e250fbb3f79db3304241c8edc0e289
Katie Soldau <ksoldau@gmail.com>
2013-05-30 15:51:42 -0400

    refactor subject link popups and dialogs

--- app/assets/javascripts/assignment_dialog.js
+++ app/assets/javascripts/assignment_dialog.js
@@ -1,58 +1,59 @@
-function initializeAssignmentsDialogs() {
-  $(".subj_link").each(
-    function() {
-      var popup = $(this).find(".a_popup").dialog({
-        closeOnEscape: true,
-        title: "Assignment",
-        draggable: false, 
-        resizable: false, 
-        modal: false,
-        autoOpen: false,
-        width: 235,
-        minHeight: 0,
-      });
+// for the assignment popups/dialogs on the month view
+$(function(){
+  //initialize the popup that shows up on hover
+  initPopUps();
+  initDialogs();
+  subjectLinksPopupsAndDialogs();
+})
 
-
-      $(this).mouseover( function() {
-        popup.dialog("open");
-        popup.parent(".ui-dialog").find(".ui-dialog-titlebar-close").css("visibility", "hidden");
-        if (popup.hasClass("completed")) {
-          popup.parent(".ui-dialog").css("background-color", "#7ECEFD");
-        }
-        else {
-          popup.parent(".ui-dialog").css("background-color", "#FF7F66");
-        }
-
-      }).mousemove(function(event) {
-        popup.dialog("option", "position", [event.clientX - 150, event.clientY - 170]);
-      }).mouseout(function() {
-        popup.dialog("close");
-      });
-
-      $(this).click( function() {
-        popup.dialog("close");
-      });
-
-      var dlg = $(this).find(".a_dialog").dialog({
-       //open: function(event, ui) { $(".ui-dialog-titlebar-close").show(); },
-        closeOnEscape: true,
-        title: "Assignment",
-        draggable: true,
-        resizable: false,
-        modal: true,
-        autoOpen: false,
-        width: '235', 
+function initPopUps() {
+    $(".a_popup").each(function() {
+        $(this).dialog({
+          closeOnEscape: true,
+          title: "Assignment",
+          draggable: false, 
+          resizable: false, 
+          modal: false,
+          autoOpen: false,
+          width: 235,
+          minHeight: 0,
         });
+    });
+}
 
-      $(this).on('click', function() {
-        dlg.dialog('open');
-        if (dlg.hasClass("completed")) {
-          dlg.parent(".ui-dialog").css("background-color", "#7ECEFD");
-        }
-        else {
-          dlg.parent(".ui-dialog").css("background-color", "#FF7F66");
-        }
-      });
+function initDialogs() {
+    $(".a_dialog").each(function() {
+        $(this).dialog({
+          closeOnEscape: true,
+          title: "Assignment",
+          draggable: true,
+          resizable: false,
+          modal: true,
+          autoOpen: false,
+          width: '235', 
+        });
+    });
+}
+
+//connect subject links with their assignment 
+// popups and dialogs
+function subjectLinksPopupsAndDialogs() {
+  $(".subj_link").each(function() {
+      
+      //get assignment of this subject link
+      assignmentId = $(this).data('assignment-id');
+      
+      //get associated assignment popup/dialog on hover
+      var popup = $(".a_popup[data-assignment-id=" + assignmentId + "]");
+      //get associated assignment dialog on click
+      var dlg = $(".a_dialog[data-assignment-id=" + assignmentId + "]");
+     
+      // when mouse over a subject link
+      mouseOverSubjectLink($(this), popup, dlg);
+     
+      // when click on a subject link
+      clickOnSubjectLink($(this), popup, dlg);
+
 
       //maybe this is how you do it
       dlg.data("trgr", $(this));
@@ -61,10 +62,56 @@ function initializeAssignmentsDialogs() {
   });
 }
 
+//what happens when mouse over a subject link
+function mouseOverSubjectLink(subjLink, popup, dlg) {
+    
+    //when mouseover subject link
+    subjLink.mouseover( function() {
+        
+        //open popup and hide visibility of its titlebar (for style reasons)
+        popup.dialog("open");
+        popup.parent(".ui-dialog").find(".ui-dialog-titlebar-close").css("visibility", "hidden");
+        
+        //correctly color background based on assignment completion
+        if (popup.hasClass("completed")) {
+          popup.parent(".ui-dialog").css("background-color", "#7ECEFD");
+        }
+        else {
+          popup.parent(".ui-dialog").css("background-color", "#FF7F66");
+        }
 
+      });
 
-$(function(){
+    //if popup open move it with mouse while mouse over subject link
+    subjLink.mousemove(function(event) {
+        popup.dialog("option", "position", [event.clientX - 150, event.clientY - 170]);
+    });
+    
+    //close the popup when mouse moves out of subject link
+    subjLink.mouseout(function() {
+        popup.dialog("close");
+    });
 
- initializeAssignmentsDialogs();
+}
 
-})
+//what happens when a subject link is clicked on
+function clickOnSubjectLink(subjLink, popup, dlg) {
+
+    subjLink.on('click', function() {
+        
+        //make sure pop up closed
+        popup.dialog("close");
+        
+        //open the dialog
+        dlg.dialog('open');
+        
+        //dialog background color based on assignment completeion
+        if (dlg.hasClass("completed")) {
+          dlg.parent(".ui-dialog").css("background-color", "#7ECEFD");
+        }
+        else {
+          dlg.parent(".ui-dialog").css("background-color", "#FF7F66");
+        }
+    });
+
+}
