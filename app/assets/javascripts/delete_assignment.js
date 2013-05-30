6d6cad60cbfae17d53fb5c72258427a68d9a75db
Katie Soldau <ksoldau@gmail.com>
2013-05-30 16:38:51 -0400

    refactor delete assignments js

--- app/assets/javascripts/delete_assignment.js
+++ app/assets/javascripts/delete_assignment.js
@@ -1,32 +1,30 @@
 $(function() {
   $(".delete_dialog").each(function() {
     var dlg = $(this);
-    var trigger = dlg.data("trigger");
-    var trigger_subj = dlg.data("trigger_subj");
+
 
     dlg.find("a").on('ajax:success', function(e) {
-      console.log("log successfully deleted");
-      /* $(".delete_dialog a").closest(".ui-dialog").first().hide(); */
-      /* $(".ui-widget-overlay").hide(); */
+      
+      //close delete dialog
       dlg.dialog('close');
-      //trigger.closest(".as_box").slideUp();
+      
+      //slide associated assignment up, hiding it
       trigger.closest(".as_box").animate({opacity: '0'}, 900, function() {
         trigger.closest(".as_box").slideUp()
         });
 
-      //find all open ui-dialogs
+      //find open dialogs and close them?
       $(".a_dialog").dialog('close');
       trigger_subj.delay(400).slideUp();
-   }).on('ajax:failure', function(e) {
-    console.log("log deletion FAILED");
-    });
+      
+      // log to console if ajax fails
+      }).on('ajax:failure', function(e) {
+      console.log("log deletion FAILED");
+      });
+    
+    //save this data for later use
+    var trigger = dlg.data("trigger");
+    var trigger_subj = dlg.data("trigger_subj");
+
   });
 })
-
-/* /1* a a char *1/ */
-/* /1* . any char *1/ */
-/* /1* a* zero or more *1/ */
-/* /1* a? zerp pr oen *1/ */
-/* /1* a+ one or more *1/ */
-/* /1* | or *1/ */
-
