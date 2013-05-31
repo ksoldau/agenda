4b1f6acdeb87bc6e18deea4b0add52a795f9e27c
Katie Soldau <ksoldau@gmail.com>
2013-05-31 14:36:32 -0400

    refactor edit assignment js

--- app/assets/javascripts/edit_assignment.js
+++ app/assets/javascripts/edit_assignment.js
@@ -82,7 +82,7 @@ function updatePlacement($assignment, data) {
     var new_day = Number(new_parsed_date[2]);
     var new_month = Number(new_parsed_date[1]) -1; //0 is jan
     var new_year = Number(new_parsed_date[0]);
-    var new_date = new Date(new_year, new_month, new_day);
+    var new_date = newDate(data); 
 
     var url = window.location.href;
     var url_parsed = url.split('=')[2].split('-');
@@ -102,9 +102,12 @@ function updatePlacement($assignment, data) {
     old_am_pm = old_time.split(' ')[3];
 
     //make old_hour on 24 hour time
+    console.log("old hour is " + old_hour);
+    console.log("old_am_pm is " + old_am_pm);
     old_hour = make24(old_hour, old_am_pm);
     
     var old_date_and_time = new Date(old_year, 0, old_day, old_hour, old_min);
+
     var new_date_and_time = new Date(new_year, 0, new_day, new_hour, new_min);
 
     var old_date = new Date(old_year, 0, old_day);
@@ -116,93 +119,21 @@ function updatePlacement($assignment, data) {
       moveBeforeWeek($assignment);
     }
 
-
     // if date moved to after week
-    if (movedAfterWeek(url_date, new_date)) {
+    else if (movedAfterWeek(url_date, new_date)) {
       moveAfterWeek($assignment);
     }
 
-
-    // if due date and time have not changed
-    else if (new_date_and_time.getTime() === old_date_and_time.getTime()) {
-      //do nothing
+    // if date time of due date did not change
+    else if (sameDateTime(new_date_and_time, old_date_and_time)) {
+      // do nothing
     }
     
     //if date or time has changed
-    else if (new_date_and_time.getTime() !== old_date_and_time.getTime()){
-      //old assignment
-      $assignment.animate({opacity: '0'}, 800, function() {
-        $assignment.slideUp(600, function (e) {
-
-      //figure out where to put new assignment
-      var new_day_string = String(new_day);
+    else if (!sameDateTime(new_date_and_time, old_date_and_time)){
+      move($assignment, data);
       
-      var dayHeadingArray = $(".day_heading");
-      for (var i = 0; i < dayHeadingArray.length; i++) {
-        h = dayHeadingArray[i];
-        htext = $(h).text();
 
-        // is it in the right day?
-        if (htext.indexOf(new_day_string + ",") >= 0) {
-          var new_ab = $(h).closest(".assignment_box");
-
-          var assignmentsArray = new_ab.find(".as_box");
-
-          //
-          $assignment.appendTo(new_ab);
-          
-          var aArray = Array.prototype.slice.call(assignmentsArray);
-          
-          if (aArray.length > 0) {
-            for (i = 0; i < aArray.length; i++) { 
-              var o_a = $(aArray[i]);
-              var o_due_time = o_a.find(".due_time");
-              
-              var o_due_time_text = o_due_time.text().replace(/\s+/g, ' ').trim();
-              
-              var o_hour = Number(o_due_time_text.split(' ')[2].split(':')[0]);
-              var o_min = Number(o_due_time_text.split(' ')[2].split(':')[1]);
-              
-              var am_or_pm = o_due_time_text.split(' ')[3];
-              
-              var o_24hour = o_hour;
-              
-              if (am_or_pm === "am" && o_hour === 12) {
-                var o_24hour = 0;
-              }
-              if (am_or_pm === "pm" && o_hour > 12) {
-                var o_24hour = o_hour + 12;
-              }
-
-              var o_date = new Date (0, 0, 1, o_24hour, o_min);
-
-              
-              if (new_hour < 10) {
-                /* new_hour = "0" + new_hour; */
-              }
-              if (new_min < 10) {
-                /* new_min = "0" + new_min; */
-              }
-              var new_date_time = new Date(0, 0, 1, Number(new_hour), Number(new_min));
-                           
-              if (new_date_time.getTime() <= o_date.getTime()) { //opposite bc negative for some reason
-                o_a.before($assignment);
-              }
-            }; //end of for
-          }
-
-          $assignment.slideDown(600, function() {
-            $assignment.animate({opacity: '1'}, 600);
-          });
-
-        } // end of if
-      
-      } 
-
-
-        });
-      });
-      
 
 
     }
@@ -259,12 +190,14 @@ function updateCompletionBackground($assignment, data) {
 // based on hour and if am or pm
 function make24(hour, amPm) {
     
-    if (old_am_pm === "am" && old_hour === 12) {
-      old_hour = 0;
+    if (amPm === "am" && hour === 12) {
+      hour = 0;
     } 
-    if (old_am_pm === "pm" && old_hour !== 12) {
-      old_hour += 12;
+    if (amPm === "pm" && hour !== 12) {
+      hour += 12;
     }
+
+    return hour;
 
 }
 
@@ -295,4 +228,175 @@ function moveAfterWeek($assignment) {
     });
 }
 
+// are these two datetimes the same?
+function sameDateTime(datetime1, datetime2) {
+    return datetime1.getTime() === datetime2.getTime();
+}
+
+// move the assignment to new place within week
+
+function move($assignment, data) {
+    var new_dd = data.due_date;
+    var new_parsed_date = new_dd.split('T')[0].split('-');
+    var new_parsed_time = new_dd.split('T')[1].split(':');
+    var new_min = Number(new_parsed_time[1]);
+    var new_hour = Number(new_parsed_time[0]);
+    var new_day = Number(new_parsed_date[2]);
+    var new_date = newDate(data);
+
+    old_date = $assignment.closest(".assignment_box").find(".day_heading").text().trim();
+    old_time = $assignment.find(".due_time").text().replace(/\s+/g, ' ').trim();
+    old_hour = Number(old_time.split(' ')[2].split(':')[0]);
+    old_min = Number(old_time.split(' ')[2].split(':')[1]);
+    old_am_pm = old_time.split(' ')[3];
+
+    //make old_hour on 24 hour time
+    console.log("old hour is " + old_hour);
+    console.log("old_am_pm is " + old_am_pm);
+    old_hour = make24(old_hour, old_am_pm);
+    
+      //close old assignment then move it and open it again
+      $assignment.animate({opacity: '0'}, 800, function() {
+        $assignment.slideUp(600, function (e) {
+          moveAssignmentAndSlideDown($assignment, data);
+
+
+
+        });
+      });
+      
+}
+
+// get the new date after assignment has been edited
+function newDate(data) {
+    var new_dd = data.due_date;
+    var new_parsed_date = new_dd.split('T')[0].split('-');
+    var new_day = Number(new_parsed_date[2]);
+    var new_month = Number(new_parsed_date[1]) -1; //0 is jan
+    var new_year = Number(new_parsed_date[0]);
+    var new_date = new Date(new_year, new_month, new_day);
+
+    return new_date;
+}
+
+// move assignment to new position and then show it
+function moveAssignmentAndSlideDown($assignment, data) {
+
+      // list of day headings (dates) on the page
+      var dayHeadingArray = $(".day_heading");
+      
+      // iterate through dates on the page
+      for (var i = 0; i < dayHeadingArray.length; i++) {
+        
+        // date in string form of day heading
+        h = dayHeadingArray[i];
+        // text from the day heading dom element
+        htext = $(h).text();
+
+        // if assignment now in this day
+        if (inRightDay(data, htext)) {
+            var new_ab = $(h).closest(".assignment_box");
+            var assignments = new_ab.find(".as_box");
+            putAssignmentInDay($assignment, assignments, data); 
+
+        } // end of if
+      
+      } 
+}
+
+// determines if day in header on page is same as the 
+// due day of assignment after its edited
+function inRightDay(data, htext) {
+  var new_dd = data.due_date;
+  var new_parsed_date = new_dd.split('T')[0].split('-');
+  var new_day = Number(new_parsed_date[2]);
+
+  var new_day_string = String(new_day);
+
+  return subString(new_day_string + ",", htext);
+}
+
+// determines if a string is a substring of another
+function subString(sub, full) {
+  return full.indexOf(sub) >= 0;
+}
+
+// put assignment in the day
+function putAssignmentInDay($assignment, other_assignments, data) {
+    var new_ab = $(h).closest(".assignment_box");
+
+    // add to day in case no other assignments exist 
+    // to compare it to
+    $assignment.appendTo(new_ab);
+    
+    // put assignment in right place in dom
+    putAssignmentInOrder($assignment, other_assignments, data);
+    
+    // show the edited assignment
+    $assignment.slideDown(600, function() {
+      $assignment.animate({opacity: '1'}, 600);
+    });
+}
+
+// put assignment in correct order
+function putAssignmentInOrder($assignment, other_assignments, data) {
+    
+    var other_assignments_array = Array.prototype.slice.call(other_assignments);
+    
+    // place assignment in order with other assignments
+    if (other_assignments_array.length > 0) {
+      for (i = 0; i < other_assignments_array.length; i++) {
+      $other_assignment =  $(other_assignments_array[i]);
+      debugger;
+        
+        // if assignment comes before another, 
+        // place it before it
+        if (dueBefore(data, $other_assignment)) {
+          $other_assignment.before($assignment);
+        }
+      }; //end of for
+    }
+}
+
+// is the due date in data before the other assignment?
+function dueBefore(data, $other_assignment) {
+  
+  // get the new time after assignment edited
+  var new_time = dataTime(data); 
+  
+  // get the time comparing assignment to
+  var other_time = otherTime($other_assignment);
+
+  return new_time.getTime() <= other_time.getTime();
+}
+
+// get the new time from the data given back from ajax
+function dataTime(data) {
+  
+  var due_date = data.due_date;
+  var parsed_time = due_date.split('T')[1].split(':');
+  var min = Number(parsed_time[1]);
+  var hour = Number(parsed_time[0]);
+
+  var time = new Date(0, 0, 1, hour, min);
+
+  return time;
+}
+
+// get the time from dom assignment
+function otherTime($other_assignment) {
+  var o_due_time = $other_assignment.find(".due_time");
+  var o_due_time_text = o_due_time.text().replace(/\s+/g, ' ').trim();
+  var o_hour = Number(o_due_time_text.split(' ')[2].split(':')[0]);
+  var o_min = Number(o_due_time_text.split(' ')[2].split(':')[0]);
+   
+  var am_pm = o_due_time_text.split(' ')[3];
+  
+  // get the hour in terms of 24
+  var o_24hour = make24(o_hour, am_pm);
+  
+  var time = new Date(0, 0, 1, o_24hour, o_min);
+  
+  return time;
+}
 
