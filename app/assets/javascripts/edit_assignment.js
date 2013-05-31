6b86469208704197861565fd121fa1a436dab169
Katie Soldau <ksoldau@gmail.com>
2013-05-31 10:41:31 -0400

    renamed dialog variable to dlg

--- app/assets/javascripts/edit_assignment.js
+++ app/assets/javascripts/edit_assignment.js
@@ -1,16 +1,16 @@
 $(function() {
   $(".edit_dialog").each(function() {
     console.log("in the each");
-    var dialog = $(this);
-    var trigger = dialog.data("trigger");
+    var dlg = $(this);
+    var trigger = dlg.data("trigger");
 
 
-    dialog.find("form").on('ajax:success', function(e, data, status, xhr) {
+    dlg.find("form").on('ajax:success', function(e, data, status, xhr) {
       console.log("this is e: %o", e);
       console.log("this is data: %o", data);
       console.log("ajax success");
       console.log("assignment successfully edited");
-      dialog.dialog('close');
+      dlg.dialog('close');
       
       //change subject if edited
       var subj = trigger.closest(".as_box").find(".subject").first();
