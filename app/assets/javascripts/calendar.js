18be9d2b169cd3f5a8f25d567653f6bd8eb5b271
Katie Soldau <ksoldau@gmail.com>
2013-06-03 09:56:00 -0400

    add add dialog to month view

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -1,8 +1,13 @@
 $(function() {
   $("td").each(
     function() {
+      var date = $(this).data('date');
+      var dlg = $(".add_dialog[data-date=" + date + "]");
       $(this).dblclick(function() {
-        alert('Handler for .dblclick() called.');
+        //alert('Handler for .dblclick() called.');
+        dlg.dialog('open');
+        
+        
         // put in dialog to add assignment to the day
       });
     }
