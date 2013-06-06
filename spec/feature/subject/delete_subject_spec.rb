386c91302df132122583bb4b169806fd6592d493
Katie Soldau <ksoldau@gmail.com>
2013-06-06 14:05:53 -0400

    test deleting a subject

---
+++ spec/feature/subject/delete_subject_spec.rb
@@ -0,0 +1,86 @@
+
+require 'spec_helper'
+
+RSpec.configure do |config|
+  config.include All::SessionSteps, type: :feature
+  config.include All::Dialogs, type: :feature
+  config.include All::Views, type: :feature
+  config.include All::Subjects, type: :feature
+  config.include Subjects::Helpers, type: :feature
+end
+
+feature 'create subject', :js => true do 
+  let(:user) { FactoryGirl.create(:user) }
+
+
+  before do
+    #sign in
+    visit '/'
+    sign_in_with(user.email, 'p@ssword')
+
+    #go to subjects page
+    subject_tab.click
+
+    add_subject('Geometry')
+
+  end
+
+  scenario 'delete a subject without assignments' do
+    # make sure has the subject Geometry
+    choose = find(".dk_toggle")
+
+    # make sure the subject Geometry exists
+    user.subjects.where(name: 'Geometry').should_not be_empty
+   
+    choose_subject('Geometry')
+
+    # delete subject
+
+    delete_subject_button.click
+
+    within ".delete_subj_dialog" do
+      find("a", :text => 'Delete Geometry').click
+    end
+
+    choose.click
+    find(".dk_options").should_not have_content("Geometry")
+    
+    # make sure subject doesn't exist anymore
+    user.subjects.where(name: 'Geometry').should be_empty
+  end
+
+  scenario 'delete a subject with assignments' do
+
+    choose = find(".dk_toggle")
+
+    # make sure subject exists
+    user.subjects.where(name: 'Geometry').should_not be_empty
+
+    # add assignments
+    add_assignment('Geometry', 'worksheet 3')
+    add_assignment('Geometry', 'worksheet 4')
+
+    #make sure assignments exist
+    user.assignments.where(description: 'worksheet 3').should_not be_empty
+    user.assignments.where(description: 'worksheet 4').should_not be_empty
+
+    # delete subject
+    choose_subject('Geometry')
+    delete_subject_button.click
+
+    within ".delete_subj_dialog" do
+      find("a", :text => 'Delete Geometry').click
+    end
+
+    #make sure that subjects assignments no longer there
+    user.assignments.where(description: 'worksheet 3').should be_empty
+    user.assignments.where(description: 'worksheet 3').should be_empty
+
+    choose.click
+
+    
+
+  end
+
+ end
+
--- spec/support/subject/helpers.rb
+++ spec/support/subject/helpers.rb
@@ -1,13 +1,28 @@
 module Subjects
   module Helpers
 
-    def choose_subject(subject)
+    def choose_subject(subj)
       find(".dk_toggle").click
-      find(".dk_options_inner").find("a", :text => subject).click
+      find(".dk_options_inner").find("a", :text => subj).click
     end
 
     def edit_subject_button
       find(".edit_subj_btn")
+    end
+
+    def delete_subject_button 
+      find(".delete_subj_btn")
+    end
+
+    def add_assignment(subj, description)
+      choose_subject(subj)
+      find(".add_btn_subj_view").click
+
+      within ".add_dialog" do
+        select_subject(subj)
+        write_description(description)
+        submit_button.click
+      end
     end
   end
 end
