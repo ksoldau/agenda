999681bf63fa6b88c58db3f278300ce78b30f0ac
Katie Soldau <ksoldau@gmail.com>
2013-06-07 09:58:46 -0400

    add cancel account dialog

---
+++ app/assets/javascripts/account.js
@@ -0,0 +1,25 @@
+$(function () {
+  initCancelAccountDialog();
+
+  associateButtonAndDialog();
+});
+
+function initCancelAccountDialog() {
+  console.log("GOT HERE");
+  $("#cancel_account_dialog").dialog( {
+    closeOnEscape: true,
+    title: "Cancel my account",
+    draggable: false,
+    resizeable: false,
+    autoOpen: false,
+    modal: true
+  });
+}
+
+// associate the cancel account button and the dialog
+function associateButtonAndDialog() {
+  $("#cancel_account_button").click( function() {
+    
+    $("#cancel_account_dialog").dialog('open');
+  });
+}
