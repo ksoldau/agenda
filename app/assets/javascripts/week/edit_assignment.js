4cffc343705fed9c8b3322ac80eacc88a190f3c4
Katie Soldau <ksoldau@gmail.com>
2013-06-11 15:49:34 -0400

    better how compare dates and times for edit assignment in week view

--- app/assets/javascripts/week/edit_assignment.js
+++ app/assets/javascripts/week/edit_assignment.js
@@ -11,19 +11,14 @@ $(function() {
     $dlg.find("form").on('ajax:success', function(e, data, status, xhr) {
       $dlg.dialog('close');
       
-      //update assignment's subject
       updateSubject($assignment, data);
       
-      //update description based on edit
       updateDescription($assignment, data);
 
-      //update of placement based on edit
       updatePlacement($assignment, data);
 
-      //update time based on edit
       updateTime($assignment, data);
    
-      //change background color based on completion
       updateCompletionBackground($assignment, data);
 
     });
@@ -81,56 +76,41 @@ function updatePlacement($assignment, data) {
     var new_year = Number(new_parsed_date[0]);
     var new_date = newDate(data); 
 
-    var url = window.location.href;
-    var url_parsed = url.split('=')[2].split('-');
-    var url_day = Number(url_parsed[2]);
-    var url_month = Number(url_parsed[1]) -1;
-    var url_year = Number(url_parsed[0]);
-    var url_date = new Date(url_year, url_month, url_day);
-    var url_date_end_week = new Date(url_date.getTime() + 6 * 24 * 60 * 60 * 1000);
+    oldDate = $assignment.closest(".assignment_box").data('date');
+    oldYear = oldDate.split('-')[0];
+    oldMonth = oldDate.split('-')[1];
+    oldDay = oldDate.split('-')[2];
 
-    old_date = $assignment.closest(".assignment_box").find(".day_heading").text().trim();
-    old_year = Number(old_date.match(/\d+/g)[1]);
-    old_day = Number(old_date.match(/\d+/g)[0]);
+    oldTime = $assignment.data('due-time');
+    oldHour = oldTime.split(':')[0];
+    oldMinute = oldTime.split(':')[1];
 
-    old_time = $assignment.find(".due_time").text().replace(/\s+/g, ' ').trim();
-    old_hour = Number(old_time.split(' ')[2].split(':')[0]);
-    old_min = Number(old_time.split(' ')[2].split(':')[1]);
-    old_am_pm = old_time.split(' ')[3];
-
-    //make old_hour on 24 hour time
-    old_hour = make24(old_hour, old_am_pm);
     
-    var old_date_and_time = new Date(old_year, 0, old_day, old_hour, old_min);
+    var old_datetime = new Date(Number(oldYear), Number(oldMonth) - 1, Number(oldDay)
+      , Number(oldHour), Number(oldMinute));
 
-    var new_date_and_time = new Date(new_year, 0, new_day, new_hour, new_min);
-
-    var old_date = new Date(old_year, 0, old_day);
-    var new_date_no_month = new Date(new_year, 0, new_day);
-
+    var new_datetime = new Date(new_year, new_month, new_day, new_hour, new_min);
 
     // if date moved to before week
-    if (movedBeforeWeek(url_date, new_date)) {
+    if (movedBeforeWeek(new_date)) {
       moveBeforeWeek($assignment);
+      debugger;
     }
 
     // if date moved to after week
-    else if (movedAfterWeek(url_date, new_date)) {
+    else if (movedAfterWeek(new_date)) {
       moveAfterWeek($assignment);
+      debugger;
     }
 
     // if date time of due date did not change
-    else if (sameDateTime(new_date_and_time, old_date_and_time)) {
+    else if (sameDateTime(new_datetime, old_datetime)) {
       // do nothing
     }
     
     //if date or time has changed
-    else if (!sameDateTime(new_date_and_time, old_date_and_time)){
-      move($assignment, data);
-      
-
-
-
+    else if (!sameDateTime(new_datetime, old_datetime)){
+     move($assignment, data);
     }
 }
 
@@ -178,6 +158,7 @@ function updateTime($assignment, data) {
 // update the background color of assignment after its been edited
 // background color signifies completion status
 function updateCompletionBackground($assignment, data) {
+
    $assignment.addClass(data.completed ? "completed" : "not_completed", {duration: 800});
    
    $assignment.removeClass(data.completed ? "not_completed" : "completed", {duration: 800});
@@ -200,8 +181,22 @@ function make24(hour, amPm) {
 }
 
 // is the date before the beginning of the week?
-function movedBeforeWeek(beginning_of_week, date) {
-  return date.getTime() < beginning_of_week.getTime();
+function movedBeforeWeek(date) {
+
+  // find first day in week
+  var $firstDay = $($(".assignment_box")[0]);
+  // date of first day
+  var firstDayDate = $firstDay.data('date');
+
+  var year = firstDayDate.split('-')[0];
+  var month = firstDayDate.split('-')[1];
+  var day = firstDayDate.split('-')[2];
+
+  beginningOfWeek = new Date(Number(year), Number(month) - 1, Number(day));
+  
+  debugger;
+
+  return date.getTime() < beginningOfWeek.getTime();
 }
 
 // move the assignment to previous week 
@@ -213,10 +208,22 @@ function moveBeforeWeek($assignment) {
 }
 
 // is the date after the end of the week?
-function movedAfterWeek(beginning_of_week, date) {
-  var end_of_week = new Date(beginning_of_week.getTime() + 6 * 24 * 60 * 60 * 1000);
+function movedAfterWeek(date) {
 
-  return date.getTime() > end_of_week.getTime();
+  // last day in the week
+  var $lastDay = $($(".assignment_box")[6]);
+  // date of last day
+  var lastDayDate = $lastDay.data('date');
+
+  var year = lastDayDate.split('-')[0];
+  var month = lastDayDate.split('-')[1];
+  var day = lastDayDate.split('-')[2];
+
+  endOfWeek = new Date(Number(year), Number(month) - 1, Number(day));
+
+  debugger; 
+
+  return date.getTime() > endOfWeek.getTime();
 }
 
 // move the assignment to the next week
@@ -234,29 +241,11 @@ function sameDateTime(datetime1, datetime2) {
 // move the assignment to new place within week
 
 function move($assignment, data) {
-    var new_dd = data.due_date;
-    var new_parsed_date = new_dd.split('T')[0].split('-');
-    var new_parsed_time = new_dd.split('T')[1].split(':');
-    var new_min = Number(new_parsed_time[1]);
-    var new_hour = Number(new_parsed_time[0]);
-    var new_day = Number(new_parsed_date[2]);
-    var new_date = newDate(data);
-
-    old_date = $assignment.closest(".assignment_box").find(".day_heading").text().trim();
-    old_time = $assignment.find(".due_time").text().replace(/\s+/g, ' ').trim();
-    old_hour = Number(old_time.split(' ')[2].split(':')[0]);
-    old_min = Number(old_time.split(' ')[2].split(':')[1]);
-    old_am_pm = old_time.split(' ')[3];
-
-    //make old_hour on 24 hour time
-    old_hour = make24(old_hour, old_am_pm);
     
-      //close old assignment then move it and open it again
-      $assignment.animate({opacity: '0'}, 800, function() {
-        $assignment.slideUp(600, function (e) {
-          moveAssignmentAndSlideDown($assignment, data);
-
-
+    //close old assignment then move it and open it again
+    $assignment.animate({opacity: '0'}, 800, function() {
+      $assignment.slideUp(600, function (e) {
+        moveAssignmentAndSlideDown($assignment, data);
 
         });
       });
@@ -279,21 +268,20 @@ function newDate(data) {
 function moveAssignmentAndSlideDown($assignment, data) {
 
       // list of day headings (dates) on the page
-      var dayHeadingArray = $(".day_heading");
+      var assignmentBoxArray = $(".assignment_box");
       
       // iterate through dates on the page
-      for (var i = 0; i < dayHeadingArray.length; i++) {
+      for (var i = 0; i < assignmentBoxArray.length; i++) {
         
-        // date in string form of day heading
-        h = dayHeadingArray[i];
-        // text from the day heading dom element
-        htext = $(h).text();
+        // the day 
+        var $day = $(assignmentBoxArray[i]);
+        // the date of the day
+        var date = $day.data('date');
 
         // if assignment now in this day
-        if (inRightDay(data, htext)) {
-            var new_ab = $(h).closest(".assignment_box");
-            var assignments = new_ab.find(".as_box");
-            putAssignmentInDay($assignment, assignments, data); 
+        if (inRightDay(data, date)) {
+            var assignments = $day.find(".as_box");
+            putAssignmentInDay($assignment, assignments, $day, data); 
 
         } // end of if
       
@@ -302,14 +290,12 @@ function moveAssignmentAndSlideDown($assignment, data) {
 
 // determines if day in header on page is same as the 
 // due day of assignment after its edited
-function inRightDay(data, htext) {
+function inRightDay(data, date) {
   var new_dd = data.due_date;
-  var new_parsed_date = new_dd.split('T')[0].split('-');
-  var new_day = Number(new_parsed_date[2]);
+  var newDate = new_dd.split('T')[0];
 
-  var new_day_string = String(new_day);
-
-  return subString(new_day_string + ",", htext);
+  debugger;
+  return date === newDate;
 }
 
 // determines if a string is a substring of another
@@ -318,15 +304,14 @@ function subString(sub, full) {
 }
 
 // put assignment in the day
-function putAssignmentInDay($assignment, other_assignments, data) {
-    var new_ab = $(h).closest(".assignment_box");
+function putAssignmentInDay($assignment, other_assignments, $day, data) {
 
     // add to day in case no other assignments exist 
     // to compare it to
     //$assignment.appendTo(new_ab);
     
     // put assignment in right place in dom
-    putAssignmentInOrder($assignment, other_assignments, data, new_ab);
+    putAssignmentInOrder($assignment, other_assignments, data, $day);
     
     // show the edited assignment
     $assignment.slideDown(600, function() {
@@ -335,13 +320,12 @@ function putAssignmentInDay($assignment, other_assignments, data) {
 }
 
 // put assignment in correct order
-function putAssignmentInOrder($assignment, other_assignments, data, new_day) {
+function putAssignmentInOrder($assignment, other_assignments, data, $day) {
     
     var other_assignments_array = Array.prototype.slice.call(other_assignments);
     
     // place assignment in order with other assignments
     if (other_assignments_array.length > 0) {
-      console.log('it went into the if')
       var beforeSomething = false;
       for (i = 0; i < other_assignments_array.length; i++) {
       $other_assignment =  $(other_assignments_array[i]);
@@ -355,12 +339,12 @@ function putAssignmentInOrder($assignment, other_assignments, data, new_day) {
         }
       }; //end of for
       if (!beforeSomething) {
-        $assignment.appendTo(new_day);
+        $assignment.appendTo($day);
       }
     }
     else {
-      $assignment.appendTo(new_day);
-      console.log("FJKDSLFNJK " + new_day.text());
+      $assignment.appendTo($day);
+      console.log("FJKDSLFNJK " + $day.text());
     }
 }
 
@@ -392,15 +376,11 @@ function dataTime(data) {
 
 // get the time from dom assignment
 function otherTime($other_assignment) {
-  var o_due_time = $other_assignment.find(".due_time");
-  var o_due_time_text = o_due_time.text().replace(/\s+/g, ' ').trim();
-  var o_hour = Number(o_due_time_text.split(' ')[2].split(':')[0]);
-  var o_min = Number(o_due_time_text.split(' ')[2].split(':')[1]);
-  var am_pm = o_due_time_text.split(' ')[3];
-  
-  // get the hour in terms of 24
-  var o_24hour = make24(o_hour, am_pm);
-  var time = new Date(0, 0, 1, o_24hour, o_min);
+  var dueTime = $other_assignment.data('due-time');
+  var hour = dueTime.split(':')[0];
+  var minute = dueTime.split(':')[1];
+
+  var time = new Date(0, 0, 1, hour, minute);
   
   return time;
 }
